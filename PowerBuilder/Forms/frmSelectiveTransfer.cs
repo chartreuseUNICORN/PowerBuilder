41f88add150e1b130280ce2915e824262317ffcf
chartreuseUNICORN <smclough@gmail.com>
2025-01-14 10:02:55 -0800

    move commands and forms to respective namespaces

--- PowerBuilder/Forms/frmSelectiveTransfer.cs
+++ PowerBuilder/Forms/frmSelectiveTransfer.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PowerBuilderUI
+namespace PowerBuilderUI.Forms
 {
     public partial class frmSelectiveTransfer : System.Windows.Forms.Form
     {
