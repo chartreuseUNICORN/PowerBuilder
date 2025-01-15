8fc400f93798d3aeb9eeb7f9cbb5632403fdfb7a
chartreuseUNICORN <smclough@gmail.com>
2025-01-15 12:53:11 -0800

    implement category Selection Filter

---
+++ PowerBuilder/CategorySelectionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerBuilder.SelectionHelpers {
+    public class CategorySelectionFilter : ISelectionFilter {
+        private BuiltInCategory _cat;
+        public CategorySelectionFilter(BuiltInCategory cat) {
+            _cat = cat;
+        }
+        public bool AllowElement(Element elem) {
+            return elem.Category.BuiltInCategory == _cat;
+        }
+        public bool AllowReference(Reference reference, XYZ position) {
+            return false;
+        }
+    }
+}
