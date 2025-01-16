6936b9f81da94e43bbbb7df9de5fe924df4eb9a1
chartreuseUNICORN <smclough@gmail.com>
2025-01-16 11:28:52 -0800

    add Dependency Mapper UI

---
+++ PowerBuilder/Forms/frmDependencyMapper.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace PowerBuilder.Forms {
+    partial class frmDependencyMapper {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAccept.Location = new System.Drawing.Point(173, 444);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(141, 28);
+            this.btnAccept.TabIndex = 0;
+            this.btnAccept.Text = "Hmm, Interesting";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(309, 426);
+            this.treeView1.TabIndex = 1;
+            // 
+            // frmDependencyMapper
+            // 
+            this.AcceptButton = this.btnAccept;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 484);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnAccept);
+            this.Name = "frmDependencyMapper";
+            this.Text = "Dependency Map";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnAccept;
+        private TreeView treeView1;
+    }
+}
