d32165460104ca238c054a448f9865ab2ad4f1da
chartreuseUNICORN <smclough@gmail.com>
2025-07-10 10:06:03 -0500

    new ToggleOrigions command, new ViewUtils with ToggleCategoryVisibility, refactored ToggleSectionBox

---
+++ PowerBuilder/Utils/ViewUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerBuilder.Utils {
+    public static class ViewUtils {
+        public static bool ToggleCategoryVisibility (ElementId categoryId, Autodesk.Revit.DB.View activeView) {
+            bool setState;
+            
+            if (activeView.IsCategoryOverridable(categoryId)) {
+                if (activeView.GetCategoryHidden(categoryId)) {
+                    setState = false;
+                }
+                else {
+                    setState = true;
+                }
+                activeView.SetCategoryHidden(categoryId, setState);
+            }
+            
+            return true;
+        }
+    }
+}
