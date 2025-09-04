7a33afc56ff95f4f4619148e4cd37e32ef076207
chartreuseUNICORN <smclough@gmail.com>
2025-09-03 22:49:25 -0500

    change namespace

---
+++ PowerBuilder/Infrastructure/DocumentScopeUpdater.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.DB.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace PowerBuilder.IUpdaters {
+    public abstract class DocumentScopeUpdater {
+
+        protected UpdaterId _uid;
+        protected AddInId _appId;
+        protected bool LoadOnStartup;
+
+        /// <summary>
+        /// Base class for IUpdaters targeting Documents
+        /// </summary>
+        public abstract void updater_OnDocumentOpened(object sender, DocumentOpenedEventArgs args);
+        
+        public virtual void updater_OnDocumentClosing (object sender, DocumentClosingEventArgs args) {
+            UpdaterRegistry.RemoveDocumentTriggers(_uid, args.Document);
+            Log.Debug($"{this.GetType().Name} | document closing event @ {args.Document.Title}");
+        }
+    }
+}
