01f28404e6f6453016f4fe578293d4c8bf0f7cd4
chartreuseUNICORN <smclough@gmail.com>
2025-09-28 21:07:57 -0500

    add ParameterLinkUpdater and DefinitionStubs.

--- PowerBuilder/IUpdaters/ParameterLinkUpdater.cs
+++ PowerBuilder/IUpdaters/ParameterLinkUpdater.cs
@@ -21,10 +21,11 @@ namespace PowerBuilder.IUpdaters {
         protected override string _description => "Enforce value equality between two parameters";
         public override bool LoadOnStartup => true;
         
-        private Dictionary<ElementId, ElementId> _linkedParameters;
+        private static List<DefinitionStub[]> _linkedParameterPairs = new List<DefinitionStub[]>();
+        private static Dictionary<ChangeType, (int index, int orderFlag)> _registeredChangeType = new Dictionary<ChangeType, (int index, int orderFlag)>();
         
         /// <summary>
-        /// Updater that enforces value equality between pairs of registered parameters
+        /// Updater that enforces value equality between pairs of registered parameters. Only BuiltInParameters and Shared Parameters may be selected.
         /// </summary>
         /// <param name="id">AddInId</param>
         public ParameterLinkUpdater(AddInId id) {
@@ -36,18 +37,27 @@ namespace PowerBuilder.IUpdaters {
             
             foreach (ElementId eid in data.GetModifiedElementIds()) {
                 Element e = doc.GetElement(eid);
-                //KeyValuePair<ElementId,ElementId> LinkedParameterIds = _linkedParameters.Where(p => data.IsChangeTriggered(eid, Element.GetChangeTypeParameter(p.Key)));
-                KeyValuePair<ElementId, ElementId>? TriggeredPair = _linkedParameters
-                    .Where(lp => data.IsChangeTriggered(eid, Element.GetChangeTypeParameter(lp.Key)))
-                    .Cast<KeyValuePair<ElementId, ElementId>?>()
-                    .FirstOrDefault();
-                ForgeTypeId Source = GetForgeTypeIdFromElementId(doc, TriggeredPair.Value.Key);
-                ForgeTypeId Sink = GetForgeTypeIdFromElementId(doc, TriggeredPair.Value.Value);
 
-                Parameter SinkParameter = e.GetParameter(Sink);
-                Parameter SourceParameter = e.GetParameter(Source);
+                ChangeType thisChangeType = _registeredChangeType
+                    .Keys
+                    .FirstOrDefault(x => data.IsChangeTriggered(eid, x));
+                int dsIndex = _registeredChangeType[thisChangeType].index;
+                int orderFlag = _registeredChangeType[thisChangeType].orderFlag;
+                DefinitionStub[] defStubs = _linkedParameterPairs[dsIndex];
 
-                Debug.WriteLine($"PLU Trigger on {eid}: {Sink.ToLabel()} -linkedTo- {Source.ToLabel()}");
+                DefinitionStub Source = defStubs[orderFlag];
+                DefinitionStub Sink = defStubs[1-orderFlag];
+                try {
+                    Parameter SinkParameter = Sink.GetElementParameter(e);
+                    Parameter SourceParameter = Source.GetElementParameter(e);
+
+                    SinkParameter.Match(SourceParameter);
+                }
+                catch (Exception ex) {
+                    Debug.WriteLine($"{ex.GetType()}|{ex.Message}");
+                }
+
+                Debug.WriteLine($"ParameterLinkUpdater Trigger on {eid}: {Sink.GetElementParameter(e).Definition.Name} -linkedTo- {Source.GetElementParameter(e).Definition.Name}");
                 //SinkParameter.Match(SourceParameter);
                 
             }
@@ -55,34 +65,18 @@ namespace PowerBuilder.IUpdaters {
 
         public override void updater_OnDocumentOpened(object sender, DocumentOpenedEventArgs args) {
 
-            //i think this has to use one updater, but configure the triggers according to the mapping file.
-            Log.Debug($"ParameterLinkUpdater\tOnDocumentOpened:{args.Document.Title}");
-            Parameter ParameterLinkerMap = args.Document.ProjectInformation.get_Parameter(new Guid("c28bbda7-5445-408f-a4ce-edbe2793ab97"));
-            Log.Debug($"ParameterLinkerMap:\t{ParameterLinkerMap.AsValueString()}");
-            if (ParameterLinkerMap != null) {
-                Log.Debug("ParameterLinkerMap found");
-                string filepath = ParameterLinkerMap.AsValueString();
-
-                
-                Dictionary<string, List<string>> WatchedParameterMap = GetParameterMapFromPath(filepath);
-
-                //ElementClassFilter TargetElementFilter = new ElementClassFilter(typeof(Autodesk.Revit.DB.Element));
-
-                foreach (KeyValuePair<string, List<string>> kvp in WatchedParameterMap) {
-                    //how is this supposed to work with a strict name map.  with a UI you would necessarily create a configurable map between parameter elements, especially in the case of 
-                    //parameterelements with duplicate names.  for MVP, just use the first result
-                    ElementId WatchedSinkParameter = GetWatchedParameterIdByName(args.Document, kvp.Key);
-
+            // TODO: add a function to add DefinitionStub pairs from a file attached to the document
+            
+            ElementClassFilter TargetElementFilter = new ElementClassFilter(typeof(FamilyInstance)); //temporary - is this part of the registration data
+            if (!args.Document.IsFamilyDocument) {
+                for (int i=0; i<_linkedParameterPairs.Count; i++) {
                     try {
-                        if (!args.Document.IsFamilyDocument && WatchedSinkParameter != null) {
-                            foreach (string source in kvp.Value) {
-                                ElementId WatchedSourceParameter = GetWatchedParameterIdByName(args.Document, kvp.Value.First());
-                                Log.Debug($"try trigger {WatchedSourceParameter.Value} as {source}");
-                                //UpdaterRegistry.AddTrigger(_uid, args.Document, TargetElementFilter, Element.GetChangeTypeParameter(WatchedSourceParameter));
-                                Log.Debug($"\tNEW TRIGGER:\t{source}=>{kvp.Key}");
-
-                                _linkedParameters.Add(WatchedSourceParameter, WatchedSinkParameter);
-                            }
+                        DefinitionStub[] defStubPair = _linkedParameterPairs[i];
+                        Log.Debug($"try trigger {defStubPair[0].GetParameterId(args.Document)} <=> {defStubPair[1].GetParameterId(args.Document)}");
+                        for (int j = 0; j < defStubPair.Length; j++) {
+                            ChangeType changeType = Element.GetChangeTypeParameter(defStubPair[j].GetParameterId(args.Document));
+                            UpdaterRegistry.AddTrigger(_uid, args.Document, TargetElementFilter, changeType);
+                            _registeredChangeType.Add(changeType, (i, j));
                         }
                     }
                     catch (Exception e) {
@@ -90,11 +84,9 @@ namespace PowerBuilder.IUpdaters {
                     }
                 }
             }
-            else Log.Error($"{this.GetUpdaterName()}\tLinker Map parameter not found");
-            UpdaterRegistry.DisableUpdater(_uid);
         }
 
-        internal Dictionary<string,List<string>> GetParameterMapFromPath (string filepath) {
+        /*internal Dictionary<string,List<string>> GetParameterMapFromPath (string filepath) {
             if (File.Exists(filepath)) {
                 Dictionary<string, List<string>> SourceParameterMap = new Dictionary<string, List<string>>();
                 string[] lines = File.ReadAllLines(filepath);
@@ -111,43 +103,10 @@ namespace PowerBuilder.IUpdaters {
             else {
                 throw new FileLoadException("Invalid File Path");
             }
-        }
+        }*/
 
-        internal ElementId GetWatchedParameterIdByName(Document doc, string name) {
-            ElementId WatchedParameterId;
-            List<Element> ParameterElementCollector = new FilteredElementCollector(doc)
-                .OfClass(typeof(ParameterElement))
-                .ToElements()
-                .Where(x => x.Name == name)
-                .ToList();
-            IList<ForgeTypeId> Bips = ParameterUtils.GetAllBuiltInParameters();
-            try { 
-                Dictionary<string, ForgeTypeId> BipMap = Bips.ToDictionary(x => LabelUtils.GetLabelForBuiltInParameter(x), x => x);
-                
-                if (BipMap.ContainsKey(name)) {
-                    WatchedParameterId = new ElementId(ParameterUtils.GetBuiltInParameter(BipMap[name]));
-                }
-                else { 
-                    WatchedParameterId = ParameterElementCollector.FirstOrDefault().Id;
-                }
-                return WatchedParameterId;
-            }
-            catch (Exception e) {
-                Debug.WriteLine(e.Message);
-                throw e;
-            }
-        }
-        internal ForgeTypeId GetForgeTypeIdFromElementId (Document doc, ElementId ParameterId) {
-            ForgeTypeId ParameterTypeId;
-            if (ParameterId.Value > 0) {
-                ParameterElement PE = doc.GetElement(ParameterId) as ParameterElement;
-                ParameterTypeId = PE.GetDefinition().GetTypeId();
-            }
-            else {
-                BuiltInParameter Bip = (BuiltInParameter)ParameterId.Value;
-                ParameterTypeId = ParameterUtils.GetParameterTypeId(Bip);
-            }
-            return ParameterTypeId;
+        internal void AddDefinitionStubPair(DefinitionStub A, DefinitionStub B) {
+            _linkedParameterPairs.Add([A, B]);
         }
     }
 }
